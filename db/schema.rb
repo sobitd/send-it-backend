74d7b091794c0db260cf4798f2268c75bb7b1ef7
sobitd <sobitdaniel@gmail.com>
2022-10-31 16:05:30 +0300

    modify params in parcel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_123622) do
-  create_table "parcels", force: :cascade do |t|
-    t.string "recipient_name"
-    t.string "recipient_address"
-    t.string "recipient_contact"
-    t.integer "weight"
-    t.string "from"
-    t.string "destination"
-    t.integer "total_cost"
-    t.string "order_status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_parcels_on_user_id"
+ActiveRecord::Schema[7.0].define(version: 20_221_031_123_622) do
+  create_table 'parcels', force: :cascade do |t|
+    t.string 'recipient_name'
+    t.string 'recipient_address'
+    t.string 'recipient_contact'
+    t.integer 'weight'
+    t.string 'from'
+    t.string 'destination'
+    t.integer 'total_cost'
+    t.string 'order_status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'user_id', null: false
+    t.index ['user_id'], name: 'index_parcels_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.string "username"
-    t.string "password_digest"
-    t.string "email_address"
-    t.boolean "isAdmin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'phone_number'
+    t.string 'username'
+    t.string 'password_digest'
+    t.string 'email_address'
+    t.boolean 'isAdmin'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "parcels", "users"
+  add_foreign_key 'parcels', 'users'
 end
