ab31b53b1c89de40689663e22e80d450f3a65d62
sobitd <sobitdaniel@gmail.com>
2022-10-31 18:09:18 +0300

    add session controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'sessions/new'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
