b35aa6bec3e3dd39d7dba6bc87383117f9328ee9
sobitd <sobitdaniel@gmail.com>
2022-10-31 15:47:24 +0300

    seed data into parcels db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,45 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Seeding'
-User.create!(first_name: 'Koni',
-             last_name: 'Bobi',
-             email_address: 'datazamp6@gmail.com',
-             phone_number: '0737423431',
-             username: 'apo',
-             password_digest: 'boilerplate5554',
-             isAdmin: true)
+# User.create!(first_name: 'Koni',
+#              last_name: 'Bobi',
+#              email_address: 'datazamp6@gmail.com',
+#              phone_number: '0737423431',
+#              username: 'apo',
+#              password_digest: 'boilerplate5554',
+#              isAdmin: true)
 
-User.create!(first_name: 'Faka',
-             last_name: 'Tobi',
-             email_address: 'datagam@gmail.com',
-             phone_number: '0733466577',
-             username: 'redo',
-             password_digest: 'boilerplate99944',
-             isAdmin: false)
+# User.create!(first_name: 'Faka',
+#              last_name: 'Tobi',
+#              email_address: 'datagam@gmail.com',
+#              phone_number: '0733466577',
+#              username: 'redo',
+#              password_digest: 'boilerplate99944',
+#              isAdmin: false)
 
-User.create!(first_name: 'Drey ',
-             last_name: 'Mwangi',
-             email_address: 'mandem@gmail.com',
-             phone_number: '0734662575',
-             username: 'preme',
-             password_digest: 'boilerplate9933',
-             isAdmin: false)
+# User.create!(first_name: 'Drey ',
+#              last_name: 'Mwangi',
+#              email_address: 'mandem@gmail.com',
+#              phone_number: '0734662575',
+#              username: 'preme',
+#              password_digest: 'boilerplate9933',
+#              isAdmin: false)
 
-User.create!(first_name: 'Brian',
-             last_name: 'Kimtai',
-             email_address: 'prpm@gmail.com',
-             phone_number: '0721466577',
-             username: 'prrred',
-             password_digest: 'boilerplatet644',
-             isAdmin: false)
+# User.create!(first_name: 'Brian',
+#              last_name: 'Kimtai',
+#              email_address: 'prpm@gmail.com',
+#              phone_number: '0721466577',
+#              username: 'prrred',
+#              password_digest: 'boilerplatet644',
+#              isAdmin: false)
 
-User.create!(first_name: 'Grace',
-             last_name: 'Mish',
-             email_address: 'deezer@gmail.com',
-             phone_number: '0712436577',
-             username: 'skrrr',
-             password_digest: 'boilerplate45944',
-             isAdmin: false)
+# User.create!(first_name: 'Grace',
+#              last_name: 'Mish',
+#              email_address: 'deezer@gmail.com',
+#              phone_number: '0712436577',
+#              username: 'skrrr',
+#              password_digest: 'boilerplate45944',
+#              isAdmin: false)
 
 # parcel_one = Parcel.create!(recipient_address: 'Moi Avenue 21',
 #                             recipient_name: 'Laban Kimtai',
